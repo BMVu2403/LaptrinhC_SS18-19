0b712d49291aeec763fe01b4f1faba121017403e
BMVu2403 <buiminhvu06hn@gmail.com>
2024-12-19 20:41:13 +0700

    Add files via upload

---
+++ ss18,19-ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct SinhVien {
+    char name[50];
+    int age;
+    char phoneNumber[15];
+};
+int main() {
+    struct SinhVien sv;
+    strcpy(sv.name, "Nguyen Van A"); 
+    sv.age = 20;                    
+    strcpy(sv.phoneNumber, "0123456789"); 
+    printf("Thong tin sinh vien:\n");
+    printf("Ho ten: %s\n", sv.name);
+    printf("Tuoi: %d\n", sv.age);
+    printf("So dien thoai: %s\n", sv.phoneNumber);
+    return 0;
+}
+
